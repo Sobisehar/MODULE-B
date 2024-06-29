f375c75cad1579483357f9064fcadc900b61b048
Sobisehar <rabiasehar0820@gmail.com>
2024-06-29 12:58:15 +0500

    react class1

---
+++ FirstReactApp/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp7DbyIlrHZo85yQlkC60PaVxr3tI1AFA",
+  authDomain: "react-work-00.firebaseapp.com",
+  projectId: "react-work-00",
+  storageBucket: "react-work-00.appspot.com",
+  messagingSenderId: "395431023028",
+  appId: "1:395431023028:web:0a8a10155b8367e21122e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ FirstReactApp/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return(
+    <div>
+      <h1>My First React App</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime odit explicabo dolores fugit fuga perferendis tempora, libero corrupti pariatur quibusdam ex! Atque animi modi eius aspernatur, tenetur nostrum optio.</p>
+      <img src="/vite.svg" alt="" />
+    </div>
+  )
+}
+
+export default App;
